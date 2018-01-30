c89a47e106cbeabfd6caf245c4f96e99d5b55c7f
Matthew Jones <mcolinj@mac.com>
2018-01-29 22:17:06 -0500

    Fix up serpentine ordering so it is WYSIWYG in array.

--- examples/PictureLights/PictureLights.ino
+++ examples/PictureLights/PictureLights.ino
@@ -12,50 +12,97 @@
 
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS      256
-#define NUMCOLORS        3
+#define NUMCOLORS        5
+#define NUMINBOUND       8
+#define NUMOUTBOUND      8
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
 // example for more information on possible values.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-int colors[NUMCOLORS];
+uint32_t colors[NUMCOLORS];
 
-byte picture[NUMPIXELS] = { 2, 2, 2, 2, 2, 2, 2, 2,
-                            1, 1, 1, 1, 1, 1, 1, 2,     // 0  - 7
-                            1, 1, 1, 1, 1, 1, 2, 1,
-                            1, 1, 1, 1, 1, 2, 1, 1,
-                            1, 1, 1, 1, 2, 1, 1, 1,
-                            1, 1, 1, 2, 1, 1, 1, 1,
-                            1, 1, 2, 1, 1, 1, 1, 1,
-                            1, 2, 1, 1, 1, 1, 1, 1,
-                            2, 1, 1, 1, 1, 1, 1, 1,
-                            1, 2, 1, 1, 1, 1, 1, 1,
-                            1, 1, 2, 1, 1, 1, 1, 1,
-                            1, 1, 1, 2, 1, 1, 1, 1,
-                            1, 1, 1, 1, 2, 1, 1, 1,
-                            1, 1, 1, 1, 1, 2, 1, 1,
-                            1, 1, 1, 1, 1, 1, 2, 1,
-                            1, 1, 1, 1, 1, 1, 1, 2,
-                            1, 1, 1, 1, 1, 1, 2, 1,
-                            1, 1, 1, 1, 1, 2, 1, 1,
-                            1, 1, 1, 1, 2, 1, 1, 1,
-                            1, 1, 1, 2, 1, 1, 1, 1,
-                            1, 1, 2, 1, 1, 1, 1, 1,
-                            1, 2, 1, 1, 1, 1, 1, 1,
-                            2, 1, 1, 1, 1, 1, 1, 1,
-                            1, 2, 1, 1, 1, 1, 1, 1,
-                            1, 1, 2, 1, 1, 1, 1, 1,
-                            1, 1, 1, 2, 1, 1, 1, 1,
-                            1, 1, 1, 1, 2, 1, 1, 1,
-                            1, 1, 1, 1, 1, 2, 1, 1,
-                            1, 1, 1, 1, 1, 1, 2, 1,
-                            1, 1, 1, 1, 1, 1, 1, 2,
-                            2, 2, 2, 2, 2, 2, 2, 2,
-                            2, 2, 2, 2, 2, 2, 2, 2 };
+// Transform the data to serpentine to make the array look like the display
+#define DATR(a, b, c, d, e, f, g, h)  h,g,f,e,d,c,b,a
+#define DATF(a, b, c, d, e, f, g, h)  a,b,c,d,e,f,g,h
+
+//
+// Define the picture (along with a column for inbound and for outbound) data
+// that could be used for shifting data into the image_data.   Not sure how I
+// would be able to use that yet, but I think it might be handy for something.
+//
+byte image_data[NUMINBOUND+NUMPIXELS+NUMOUTBOUND] = 
+                          { DATF(1, 1, 1, 1, 1, 1, 1, 1),   // InBound buffer of 8
+                            DATR(1, 1, 1, 1, 1, 1, 1, 1),
+                            DATF(1, 1, 1, 1, 1, 1, 1, 1),
+                            DATR(1, 1, 1, 1, 1, 1, 1, 1),
+                            DATF(1, 1, 1, 1, 1, 1, 1, 1),
+                            DATR(0, 0, 1, 1, 1, 0, 1, 1),
+                            DATF(0, 0, 0, 0, 0, 0, 0, 1),
+                            DATR(0, 0, 0, 0, 0, 0, 0, 0),
+                            DATF(0, 0, 0, 0, 0, 0, 0, 0),
+                            DATR(0, 0, 1, 1, 1, 1, 1, 1),
+                            DATF(1, 1, 1, 1, 1, 1, 1, 1),
+                            DATR(1, 0, 0, 0, 0, 0, 0, 1),
+                            DATF(0, 0, 0, 0, 0, 0, 0, 0),
+                            DATR(0, 0, 2, 2, 2, 2, 0, 0),
+                            DATF(0, 0, 2, 2, 2, 2, 0, 0),
+                            DATR(0, 0, 0, 0, 0, 0, 0, 0),
+                            DATF(1, 0, 0, 0, 0, 0, 0, 1),
+                            DATR(1, 1, 1, 1, 1, 1, 1, 1),
+                            DATF(1, 1, 1, 1, 1, 1, 1, 1),
+                            DATR(1, 1, 1, 1, 1, 1, 1, 1),
+                            DATF(1, 1, 1, 1, 1, 1, 1, 1),
+                            DATR(1, 1, 1, 1, 1, 1, 1, 1),
+                            DATF(1, 1, 1, 1, 1, 1, 1, 1),
+                            DATR(1, 1, 1, 1, 1, 1, 1, 1),
+                            DATF(1, 1, 1, 1, 1, 1, 1, 1),
+                            DATR(1, 1, 1, 1, 1, 1, 1, 1),
+                            DATF(1, 1, 1, 1, 1, 1, 1, 1),
+                            DATR(1, 1, 1, 1, 1, 1, 1, 1),
+                            DATF(1, 1, 1, 1, 1, 1, 1, 1),
+                            DATR(1, 1, 1, 1, 1, 1, 1, 1),
+                            DATF(1, 1, 1, 1, 1, 1, 1, 1),
+                            DATR(1, 1, 1, 1, 1, 1, 1, 1),
+                            DATF(1, 1, 1, 1, 1, 1, 1, 1),
+                            DATR(2, 2, 2, 2, 2, 2, 2, 2), };  // Outbound buffer of 8
+
+// 8 element array of stuff to be shifted in
+byte *inbound = image_data;
+// N element array containing the picture data
+byte *picture = image_data+NUMINBOUND;
+// 8 element array of stuff that was just shifted out
+byte *outbound = image_data+NUMINBOUND+NUMPIXELS;
                             
+void loadSerpentinePicture(Adafruit_NeoPixel &pixels, byte *picture) {
+  //
+  // If data is stored in Serpentine form, then we can just
+  // load it in.  If the DATF and DATR macros are used, then
+  // the array is in serpentine order.
+  //
+  for (int i=0; i<NUMPIXELS; i++) {
+    pixels.setPixelColor(i, colors[picture[i]]);
+  }
+
+  pixels.show();
+}
 
 void loadPicture(Adafruit_NeoPixel &pixels, byte *picture) {
+  loadSerpentinePicture(pixels, picture);
+}
+
+
+int delayval = 1;
+
+#define NUMROWS 8
+
+//
+//  This loads an array by providing the serpentine transform
+//  as it is loaded.   Every other line has to be reversed as
+//  it is loaded in the pixel matrix.
+//
+void loadArrayPicture(Adafruit_NeoPixel &pixels, byte *picture) {
 
   int num_groups = pixels.numPixels() / 16;
 
@@ -77,10 +124,9 @@ void loadPicture(Adafruit_NeoPixel &pixels, byte *picture) {
   pixels.show();
 }
 
-int delayval = 1;
-
-#define NUMROWS 8
-
+//
+// Reading image from pixel memory, shift it off to the right
+//
 void shiftPicture(Adafruit_NeoPixel &pixels, int how_far) {
   for(int r=31; r>=0; r--) {
     shiftRow(pixels, r);
@@ -104,9 +150,11 @@ void shiftRow(Adafruit_NeoPixel &pixels, int group) {
 void setup() {
   pixels.begin(); // This initializes the NeoPixel library.
 
-  colors[0] = pixels.Color(0,  0,  0);
-  colors[1] = pixels.Color(5,  0,  5);
-  colors[2] = pixels.Color(0,  5,  0);
+  colors[0] = pixels.Color(0,  0,  3);
+  colors[1] = pixels.Color(2,  1,  0);    //  red
+  colors[2] = pixels.Color(0,  2,  0);    //  green (zig zag?)
+  colors[3] = pixels.Color(0,  0,  3);    //  blue
+  colors[4] = pixels.Color(5,  5,  0);
 
   Serial.begin(9600);
 
@@ -116,6 +164,5 @@ void setup() {
 
 void loop() {
     // shift it down forever
-    shiftPicture(pixels, 1);
-    delay(1);
+    // shiftPicture(pixels, 1);
 }
