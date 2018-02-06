d6fd9050d512769150fc991fe9230ed15f756d5f
Matthew Jones <mcolinj@mac.com>
2018-02-06 11:54:07 -0500

    Make changes to PictureLights to work with new font scheme.

--- examples/PictureLights/PictureLights.ino
+++ examples/PictureLights/PictureLights.ino
@@ -3,11 +3,14 @@
 // render shifting text.  (with bit font definitions)
 
 #include <Adafruit_NeoPixel.h>
-#include "FontLetter.h"
+#include "PaletteFont.h"
 
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
+
+//#include <SPI.h>
+//#include <SD.h>
 
 
 // Which pin on the Arduino is connected to the NeoPixels?
@@ -15,28 +18,20 @@
 #define PIN            6
 
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS      256
-#define PIXELSPERCOLUMN  8
+#define NUMPIXELS         256
+#define PIXELSPERCOLUMN     8
 #define NUMCOLUMNS       (256/8)
-#define NUMCOLORS        5
+#define NUMCOLORS           5
 
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
 // example for more information on possible values.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Palette palette(String('mono'));
+PaletteFont font(String('Samurai3'));
 
 uint32_t colors[NUMCOLORS];
-
-byte fontbits[] =                 { 0b11111000,   // F8
-                                    0b11111110,
-                                    0b00110011,
-                                    0b00110011,
-                                    0b11111110,
-                                    0b11111000  };
-
-FontLetter letA = FontLetter('A', fontbits, 6);
-
 
 // Transform the data to serpentine to make the array look like the display
 #define DATR(a, b, c, d, e, f, g, h)  h,g,f,e,d,c,b,a
@@ -48,6 +43,7 @@ FontLetter letA = FontLetter('A', fontbits, 6);
 // would be able to use that yet, but I think it might be handy for something.
 
 
+/*
 byte picture[NUMPIXELS] = 
                           {    // InBound buffer of 8
                             DATR(1, 1, 1, 1, 1, 1, 1, 1),
@@ -81,7 +77,7 @@ byte picture[NUMPIXELS] =
                             DATR(0, 0, 0, 0, 0, 1, 0, 0),
                             DATF(1, 0, 0, 0, 1, 1, 0, 1),
                             DATR(1, 1, 1, 1, 1, 1, 1, 1),
-                            DATF(1, 1, 1, 1, 1, 1, 1, 1) };  // Outbound buffer of 8
+                            DATF(1, 1, 1, 1, 1, 1, 1, 1) };  // Outbound buffer of 8  */
 
                             
 void loadSerpentinePicture(Adafruit_NeoPixel &pixels, byte *picture) {
@@ -131,7 +127,7 @@ void loadArrayPicture(Adafruit_NeoPixel &pixels, byte *picture) {
 //
 // Reading image from pixel memory, shift it off to the right
 //
-void shiftPicture(Adafruit_NeoPixel &pixels, int how_far, byte shift_in_column, uint32_t color) {
+void shiftPicture(Adafruit_NeoPixel &pixels, byte shift_in_column, uint32_t color) {
   for(int r=0; r<32; r++) {
     shiftColumn(pixels, r);
   }
@@ -150,6 +146,29 @@ void shiftPicture(Adafruit_NeoPixel &pixels, int how_far, byte shift_in_column, 
 
   pixels.show();
   delay(33);
+}
+
+
+//
+// Reading image from pixel memory, shift it off to the right
+//
+void shiftPicture(Adafruit_NeoPixel &pixels, byte *shift_in_column, Palette &pal) {
+  for(int r=0; r<32; r++) {
+    shiftColumn(pixels, r);
+  }
+  //
+  //  Shift in some data into the first column (vacated by the shift)
+  //  It is represented as a byte instead of an array...
+  //
+  for(int i=0; i<PIXELSPERCOLUMN; i++) {
+      Serial.print(shift_in_column[i]);
+      Serial.print(" ");
+      pixels.setPixelColor(NUMPIXELS-i-1, pal.color(shift_in_column[i]));
+  }
+  Serial.println("end of column");
+
+  pixels.show();
+
 }
 
 void shiftColumn(Adafruit_NeoPixel &pixels, int column) {
@@ -174,7 +193,8 @@ void shiftColumn(Adafruit_NeoPixel &pixels, int column) {
 //
 //  Rotate the font 90 degrees to the left.   This makes
 //  it suitable for shifting in from the right (instead of
-//  from above.)
+//  from above.)   This was used initially for 8x8_basic.  Here for
+//  posterity if it is needed for another bit-data kind of font.
 //
 void rotate_font(byte *font, byte *rotated) {
   byte sel = 0x1;    // select the 1 bit from each and shift into result
@@ -203,46 +223,66 @@ void setup() {
   Serial.begin(9600);
 
   // For a set of NeoPixels the first NeoPixel is 0, second is 1
-  loadPicture(pixels, picture);
-  delay(3000);
+  //loadPicture(pixels, picture);
+  //delay(3000);
+
+  // This is the CS pin on the 5100 board.
+  //SD.begin(4);
+
+  // Ths is required for MEGA compatibility with the SD reader?
+  //pinMode(SS, OUTPUT);
+
+  
 }
 
 //
-//  Note:   Font definition is still a global imported with the FontLetter class.
-//  Perhaps we can do a better job and encapsulate the font in a proper class.
-//  Class could provide a way to set the color for all following uses of the font,
-//  and could hide things like the -32 offset and the array representation...
 //
-void shiftInMessage(Adafruit_NeoPixel &pixels, char *message, uint32_t msg_size) {
-    for (int letter = 0; letter < msg_size; letter++) {
-      byte *glyph = font8x8_1076[message[letter]-32];
+void shiftInMessage(Adafruit_NeoPixel &pixels, char *message) {
+    for (int letter_index = 0; message[letter_index]!=NULL; letter_index++) {
+      FontGlyph *glyph = font.glyph(message[letter_index]);
 
       // shift in the glyph one column at a time
       for (int col=0; col<8; col++) {
-        shiftPicture(pixels, 1, glyph[col], pixels.Color(50, 20, 0));
+        shiftPicture(pixels, glyph->column(col), pixels.Color(5, 0, 5));
       }
+      delay(33);
     }
 }
 
 void loop() {
+
+  /*File myFile;
+
     // shift it down forever (pad with blank column before and after)
     //for (byte *each_column = letA.first_column();
     //           each_column != NULL;
     //           each_column = letA.next_column()) {
     //    shiftPicture(pixels, 1, *each_column, pixels.Color(7, 0, 7));
     //}
-    char msg[] = "I'm sorry, Dave. I'm afraid I can't do that.";
-    shiftInMessage(pixels, msg, sizeof(msg));
+  // re-open the file for reading:
+  myFile = SD.open("test.txt");
 
-    delay(2000);
+  if (myFile) {
+    Serial.println("test.txt:");
 
-    char msg2[] = "Wow! that was fast!";
-    shiftInMessage(pixels, msg2, sizeof(msg2));
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      char buffer[32];
+      int bytes_read = myFile.read(buffer, sizeof(buffer)-1);
+      buffer[bytes_read] = NULL;        // make sure it is NULL terminated
+      shiftInMessage(pixels, buffer);
+    }
 
-    delay(2000);
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  } */
 
-    char msg3[] = "Ouch!!!!";
-    shiftInMessage(pixels, msg3, sizeof(msg3));
+  char *msg = "0";
+  shiftInMessage(pixels, msg);
 
+  delay(2000);
 
 }
