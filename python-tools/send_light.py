822a065bfbed5b2751b5dc5724344285ecf8722f
Helena <helenasheinonen@gmail.com>
2018-02-11 18:11:30 -0500

    takes arguments for light show

--- python-tools/send_light.py
+++ python-tools/send_light.py
@@ -1,9 +1,10 @@
 import json
 import socket
+import sys
 
 UDP_IP = "10.10.76.17"
 UDP_PORT = 8888
-MESSAGE = {'message': 'light-show', 'show-index': 6}
+MESSAGE = {'message': 'light-show', 'show-index': sys.argv[1]}
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 message = json.dumps(MESSAGE).encode("utf8")
