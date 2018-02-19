ba02587484f611fe0959c3b81660598412b89585
Matthew Jones <mcolinj@mac.com>
2018-02-19 18:21:50 -0500

    Clean up comments.  Fix off-by-one.

--- examples/EncoderTest/EncoderTest.ino
+++ examples/EncoderTest/EncoderTest.ino
@@ -1,47 +1,27 @@
 /*
-  Analog Input
+  Test CIMCoder channel.
 
-  Demonstrates analog input by reading an analog sensor on analog pin 0 and
-  turning on and off a light emitting diode(LED) connected to digital pin 13.
-  The amount of time the LED will be on and off depends on the value obtained
-  by analogRead().
+  Test a single channel of the CIMCoder attached to pin 2.
+  Measure the time at the first and the PULSE_COUNTth pulse.
 
-  The circuit:
-  - potentiometer
-    center pin of the potentiometer to the analog input 0
-    one side pin (either one) to ground
-    the other side pin to +5V
-  - LED
-    anode (long leg) attached to digital output 13
-    cathode (short leg) attached to ground
-
-  - Note: because most Arduinos have a built-in LED attached to pin 13 on the
-    board, the LED is optional.
-
-  created by David Cuartielles
-  modified 30 Aug 2011
-  By Tom Igoe
-
-  This example code is in the public domain.
+  Report results in time per 1000 pulses.  (50 revolutions @ 20 pulses / rev)
 
   http://www.arduino.cc/en/Tutorial/AnalogInput
 */
-#include <SPI.h>         // needed for Arduino versions later than 0018
 #include <Arduino1076.h>
 
+#define PULSE_COUNT    999
 int interruptPin = 2; 
 uint32_t volatile startPulse = 0;
 uint32_t volatile endPulse = 0;
 uint32_t volatile pulseCount = 0;
 
 void pulseChange() {
-   //Serial.print("Interrupt: ");
    if (digitalRead(interruptPin) == HIGH){
-      //Serial.println("HIGH");
       if (pulseCount == 0) {
         startPulse = millis();
       }
-      if (pulseCount == 1000) {
+      if (pulseCount == PULSE_COUNT) {
         endPulse = millis();
       }
       pulseCount++;
@@ -56,7 +36,7 @@ void setup() {
 }
 
 void loop() {
-  if (pulseCount > 1000) {
+  if (pulseCount > PULSE_COUNT) {
     Serial.print("Pulses: ");
     Serial.print(pulseCount);
     Serial.print("in time: ");
